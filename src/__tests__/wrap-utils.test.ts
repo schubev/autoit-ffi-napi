975f2ed1eafe27fc0b0bd87dc00081869bbbf2e5
Victor Schubert <v@schu.be>
2020-02-07 18:00:47 +0100

    add helper for outwstr

--- src/__tests__/wrap-utils.test.ts
+++ src/__tests__/wrap-utils.test.ts
@@ -1,13 +1,29 @@
-import { inWstrOfString } from '../wrap-utils'
+import { inWstrOfString, stringOfOutWstr } from '../wrap-utils'
 
 describe(inWstrOfString, () => {
   it.each([
-    ['', [0x00, 0x00]],
-    ['*', [0x2a, 0x00, 0x00, 0x00]],
-    ['**', [0x2a, 0x00, 0x2a, 0x00, 0x00, 0x00]],
-    ['test', [0x74, 0x00, 0x65, 0x00, 0x73, 0x00, 0x74, 0x00, 0x00, 0x00]],
-    ['téẞt', [0x74, 0x00, 0xe9, 0x00, 0x9e, 0x1e, 0x74, 0x00, 0x00, 0x00]],
+    ['', '0000'],
+    ['*', '2a000000'],
+    ['**', '2a002a000000'],
+    ['test', '74006500730074000000'],
+    ['téẞt', '7400e9009e1e74000000'],
   ])('%p encodes to %p', (input, expected) => {
-    expect(inWstrOfString(input)).toEqual(Buffer.from(expected))
+    expect(inWstrOfString(input)).toEqual(Buffer.from(expected, 'hex'))
+  })
+})
+
+describe(stringOfOutWstr, () => {
+  it.each([
+    ['', ''],
+    ['0000', ''],
+    ['2a00', '*'],
+    ['2a000000', '*'],
+    ['2a0000000000', '*'],
+    ['2a0000002a000000', '*'],
+    ['3ed884dd0000', '🦄'],
+    ['3ed884dd75006e00690063006f0072006e00', '🦄unicorn'],
+    ['3ed884dd75006e00690063006f0072006e000000', '🦄unicorn'],
+  ])('%p decodes to %p', (input, expected) => {
+    expect(stringOfOutWstr(Buffer.from(input, 'hex'))).toEqual(expected)
   })
 })
--- src/wrap-utils.ts
+++ src/wrap-utils.ts
@@ -7,3 +7,10 @@ export function inWstrOfString(string: string): Buffer {
   buffer.writeUInt16LE(0x0000, bufferLength - 2)
   return buffer
 }
+
+export function stringOfOutWstr(buffer: Buffer): string {
+  const rawString = buffer.toString('utf16le')
+  const rawStringEnd = rawString.indexOf('\u0000')
+  if (rawStringEnd === -1) return rawString
+  else return rawString.slice(0, rawStringEnd)
+}
