d5accd223eecd68ac01ab43bc6ef67accfb9504e
Victor Schubert <v@schu.be>
2020-02-07 18:00:56 +0100

    add some example bindings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export { lib } from './lowlevel'
+import { inWstrOfString, stringOfOutWstr } from './wrap-utils'
+import { lib } from './lowlevel'
+
+export function clipPut(clip: string): Promise<void> {
+  return new Promise(resolve => {
+    const clipBuffer = inWstrOfString(clip)
+    lib.AU3_ClipPut.async(clipBuffer, resolve)
+  })
+}
+
+export function clipGet(clipSize = 0x200): Promise<string> {
+  return new Promise(resolve => {
+    const clipBuffer = Buffer.alloc(clipSize * 2)
+    lib.AU3_ClipGet.async(clipBuffer, clipSize, () => {
+      resolve(stringOfOutWstr(clipBuffer))
+    })
+  })
+}
