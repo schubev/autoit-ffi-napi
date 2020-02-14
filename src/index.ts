924e32c724a691454e98da207943fd92c6a54080
Victor Schubert <v@schu.be>
2020-02-14 14:56:54 +0100

    add controlClickByHandle

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import { inWstrOfString, stringOfOutWstr } from './wrap-utils'
 import { MouseButton } from './functions'
 import { lib } from './lowlevel'
 
+export interface Hwnd {
+  _tag: 'hwnd'
+}
+
+function numberOfHwnd(hwnd: Hwnd): number {
+  return hwnd as any
+}
+function hwndOfNumber(hwnd: number): Hwnd {
+  return hwnd as any
+}
+
 export function clipPut(clip: string): Promise<void> {
   return new Promise(resolve => {
     const clipBuffer = inWstrOfString(clip)
@@ -26,20 +37,44 @@ export function controlClick(
   numClicks = 1,
   nX = -0x7fff_ffff,
   nY = -0x7fff_ffff,
-): Promise<number> {
-  if (window.type !== 'string') window = descriptor(window)
+): Promise<boolean> {
+  const windowString = typeof window === 'string' ? window : descriptor(window)
   return new Promise(resolve => {
-    const windowBuffer = inWstrOfString(window)
+    const windowStringBuffer = inWstrOfString(windowString)
     const textBuffer = inWstrOfString(text)
     const buttonBuffer = inWstrOfString(button)
     lib.AU3_ControlClick.async(
-      windowBuffer,
+      windowStringBuffer,
       textBuffer,
       buttonBuffer,
       numClicks,
       nX,
       nY,
-      resolve,
+      (status: number) => {
+        resolve(Boolean(status))
+      },
+    )
+  })
+}
+
+export function controlClickByHandle(
+  hwnd: Hwnd,
+  button = MouseButton.Default,
+  numClicks = 1,
+  nX = -0x7fff_ffff,
+  nY = -0x7fff_ffff,
+): Promise<boolean> {
+  return new Promise(resolve => {
+    const buttonBuffer = inWstrOfString(button)
+    lib.AU3_ControlClickByHandle.async(
+      numberOfHwnd(hwnd),
+      buttonBuffer,
+      numClicks,
+      nX,
+      nY,
+      (status: number) => {
+        resolve(Boolean(status))
+      },
     )
   })
 }
